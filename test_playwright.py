e8646b177737082d12ff0a9e93dc3329288063b8
keepdaping <keepdaping@gmail.com>
2026-03-12 00:11:32 +0300

    changes

---
+++ test_playwright.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import sync_playwright
+import time
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+
+    page = browser.new_page()
+
+    page.goto("https://x.com")
+
+    print("Browser opened successfully")
+
+    time.sleep(20)
+
+    browser.close()
