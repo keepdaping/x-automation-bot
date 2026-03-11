619306b715062b8a358375eb988ab3b2902eee12
keepdaping <keepdaping@gmail.com>
2026-03-12 00:55:41 +0300

    changes

--- browser/browser_manager.py
+++ browser/browser_manager.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 from config import HEADLESS
 
+
 class BrowserManager:
 
     def start(self):
@@ -9,10 +10,18 @@ class BrowserManager:
 
         browser = self.p.chromium.launch(
             headless=HEADLESS,
-            args=["--disable-blink-features=AutomationControlled"]
+            args=[
+                "--disable-blink-features=AutomationControlled",
+                "--no-sandbox",
+                "--disable-dev-shm-usage"
+            ]
         )
 
-        context = browser.new_context()
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+            viewport={"width": 1280, "height": 800},
+            locale="en-US"
+        )
 
         page = context.new_page()
 
