00d01cf7a4a99ecfd515708040a8ddfdc63b9272
keepdaping <keepdaping@gmail.com>
2026-03-12 00:14:35 +0300

    changes

--- test_playwright.py
+++ test_playwright.py
@@ -2,11 +2,12 @@ from playwright.sync_api import sync_playwright
 import time
 
 with sync_playwright() as p:
+
     browser = p.chromium.launch(headless=False)
 
     page = browser.new_page()
 
-    page.goto("https://x.com")
+    page.goto("https://x.com", timeout=60000, wait_until="domcontentloaded")
 
     print("Browser opened successfully")
 
