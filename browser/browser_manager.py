e41f2cbce46aff94070e8427f2b2de37b5fb086b
keepdaping <keepdaping@gmail.com>
2026-03-12 08:35:25 +0300

    changes

--- browser/browser_manager.py
+++ browser/browser_manager.py
@@ -1,17 +1,22 @@
 from playwright.sync_api import sync_playwright
 
+CHROME_PROFILE = r"C:\Users\MAI-WAY\AppData\Local\Google\Chrome\User Data"
+
 class BrowserManager:
 
     def start(self):
 
         self.p = sync_playwright().start()
 
-        browser = self.p.chromium.launch(headless=True)
-
-        context = browser.new_context(
-            storage_state="session.json"
+        context = self.p.chromium.launch_persistent_context(
+            user_data_dir=CHROME_PROFILE,
+            channel="chrome",
+            headless=False,
+            args=["--start-maximized"]
         )
 
-        page = context.new_page()
+        page = context.pages[0] if context.pages else context.new_page()
+
+        page.goto("https://x.com")
 
         return page
