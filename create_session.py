932dfc3042c9a7b38a98cd280a4b5843325d4c66
keepdaping <keepdaping@gmail.com>
2026-03-12 03:00:07 +0300

    changes

--- create_session.py
+++ create_session.py
@@ -20,3 +20,28 @@ with sync_playwright() as p:
     print("Session saved to session.json")
 
     browser.close()
+from playwright.sync_api import sync_playwright
+
+CHROME_PROFILE_PATH = r"C:\Users\MAI-WAY\AppData\Local\Google\Chrome\User Data"
+
+with sync_playwright() as p:
+    
+
+
+    context = p.chromium.launch_persistent_context(
+        user_data_dir=CHROME_PROFILE_PATH,
+        channel="chrome",
+        headless=False
+    )
+
+    page = context.pages[0] if context.pages else context.new_page()
+
+    page.goto("https://x.com")
+
+    print("Login using your normal Chrome session.")
+
+    input("Press ENTER to save cookies...")
+
+    context.storage_state(path="session.json")
+
+    print("Session saved to session.json")
