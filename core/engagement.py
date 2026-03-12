44e4217931d3ab96b8daa2cb4af5c63ffdcc468b
keepdaping <keepdaping@gmail.com>
2026-03-12 10:09:37 +0300

    changes

--- core/engagement.py
+++ core/engagement.py
@@ -4,10 +4,10 @@ from search.search_tweets import search_tweets
 from actions.like import like_tweet
 from actions.reply import reply_tweet
 from actions.follow import follow_user
-from core.generator import generate_reply
+
+from core.generator import generate_contextual_reply
 from utils.tweet_metrics import get_tweet_metrics
 from utils.engagement_score import score_tweet
-from core.generator import generate_contextual_reply
 from utils.tweet_text import get_tweet_text
 
 
@@ -16,22 +16,29 @@ def run_engagement(page):
     tweets = search_tweets(page, "AI")
 
     for tweet in tweets:
-        metrics = get_tweet_metrics(tweet)
 
+        metrics = get_tweet_metrics(tweet)
         score = score_tweet(metrics)
 
         print("Metrics:", metrics, "Score:", score)
-        
-        
-        if score < 20:
+
+        # skip extremely dead tweets
+        if score < 1:
             continue
 
-        like_tweet(tweet)
+        # like tweets frequently
+        if random.random() < 0.6:
+            like_tweet(tweet)
 
-        reply = generate_reply("")
+        # reply sometimes
+        if random.random() < 0.25:
 
-        reply_tweet(page, tweet, reply)
-        
-        if random.random() < 0.3:
-            
+            tweet_text = get_tweet_text(tweet)
+
+            reply = generate_contextual_reply(tweet_text)
+
+            reply_tweet(page, tweet, reply)
+
+        # follow sometimes
+        if random.random() < 0.15:
             follow_user(tweet)
